865df5cd1c74d531f3f9c09079acfb940e422130
Vembiah Haran MTTS <haran.vembiah@mt.com>
2023-12-04 15:28:38 +0530

    Added a duplicate function on maths.divide.py

--- maths/divide.py
+++ maths/divide.py
@@ -10,3 +10,16 @@ def divide_nums(num1, num2):
     """
     answer = num1 / num2
     return answer
+
+def divide_nums1(num1, num2):
+    """This method will be used to divide num1 by num2
+
+        :param int num1: The first number
+        :param int num2: The second number
+
+        :returns: The answer
+
+        :rtype: int
+    """
+    answer = num1 / num2
+    return answer
