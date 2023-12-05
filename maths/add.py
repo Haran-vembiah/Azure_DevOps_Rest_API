402b02c51a1e40ad23a9ab0a1bcc56fe5f06383f
Vembiah Haran MTTS <haran.vembiah@mt.com>
2023-12-05 22:21:37 +0530

    Removed duplicate function from add.py

--- maths/add.py
+++ maths/add.py
@@ -11,16 +11,3 @@ def add_nums(num1, num2):
     answer = num1 + num2
     return answer
 
-
-def add_nums1(num1, num2):
-    """This method will be used to add two numbers on another method
-
-        :param int num1: The first number
-        :param int num2: The second number
-
-        :returns: The sum of two numbers
-
-        :rtype: int
-    """
-    answer = num1 + num2
-    return answer
