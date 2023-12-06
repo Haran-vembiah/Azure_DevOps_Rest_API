1d14071a8602674f13998f34a97259b702093910
Vembiah Haran MTTS <haran.vembiah@mt.com>
2023-12-06 21:27:27 +0530

    config changes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -44,28 +44,15 @@ templates_path = [
 html_sidebars = {
     '**': [
         'versioning.html',
-        # 'sidebar/sphinx_versioning.html',
         # 'page.html',
         # 'versions_and_tags.html',
         'searchbox.html',
         'globaltoc.html',
         'relations.html',
         'sourcelink.html',
-        # 'scv.html'
     ],
 }
 
-html_theme_options = {
-    "versions": {"latest": "/latest", "v1.0": "/v1.0", "v2.0": "/v2.0"},
-    "prev_next_buttons_location": "both",
-    "style_external_links": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-    "display_version_dropdown": True,
-    "version_dropdown_title": "Versions & Tags",
-    "version_dropdown": "versions_and_tags.html",
-}
 html_theme = 'classic'
 html_static_path = ['_static']
 
